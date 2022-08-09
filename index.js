21d7e436bf4298a43d93cb7209ac576285ce49bd
fiction <footedroom575@gmail.com>
2022-08-09 23:11:05 +0500

    commented code

--- index.js
+++ index.js
@@ -5,24 +5,28 @@ const cors = require('cors');
 let bodyParser = require("body-parser")
 const app = express();
 
+// object constructor for storing url information
 function URL(original, short){
   this.short_url = short
   this.original_url = original
 }
 
+// this list will hold the data of all shortened links, no database is implemented
 let list_urls = []
 
-// Basic Configuration
+// Basic Configuration for port
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : false}))
+
+// middleware to log every request, helps in debugging
 app.use(function(req, res, next){
   console.log(req.method, req.path, req.body, req.params)
   next()
 })
 
-
+// function that generates unique ID every time its called
 function makeid(length) {
   var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -34,6 +38,7 @@ charactersLength));
  return result+Date.now();
 }
 
+// for accessing shortened urls
 app.get("/api/shorturl/:url", (req, res)=>{
   let shortened = req.params.url
   let original = list_urls.filter((val)=>{
@@ -47,13 +52,14 @@ app.get("/api/shorturl/:url", (req, res)=>{
   } else {
     res.redirect(original[0].original_url)
   }
-
 })
 
+// for storing shortened URLs
 app.post("/api/shorturl/", (req, res)=>{
   let shortened = makeid(5)
   let original = req.body.url
 
+  // regex for validating standard URL syntax
   const urlPattern = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
   if (!urlPattern.test(original)){
     res.json({ "error": 'invalid url' })
